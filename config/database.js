2fbdae963c9193a23c93c8e5385bf7bcbf9b6176
Sean Darcy <darcys22@gmail.com>
2015-10-18 22:22:12 +1100

    bleh gitignro

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb://<dbuser>:<dbpassword>@ds043467.mongolab.com:43467/stock' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
