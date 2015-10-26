fe9e019bf7f41509f1a27dc41576910fb9de2fb3
Sean Darcy <darcys22@gmail.com>
2015-10-26 23:33:40 +1100

    moving slowly, searchs fro teh owned shares and adds the info

--- public/app/controllers/portfolio.js
+++ public/app/controllers/portfolio.js
@@ -8,6 +8,15 @@ var yqlBuilder = function (quotes) {
   return "http://query.yahooapis.com/v1/public/yql?q=" + escape(query) + "&format=json&env=http://datatables.org/alltables.env";
 };
 
+var arrayObjMerger = function (arr1, arr2) {
+  for(var i in arr1){
+    for (var j in arr2)
+      if (arr2[j].symbol == arr1[i].name) {
+        for (var attrname in arr2[j]) {arr1[i][attrname] = arr2[j][attrname];}
+      }
+  }
+};
+
 angular.module('myApp.portfolio', ['ngRoute'])
 
 // Declared route
@@ -47,20 +56,12 @@ angular.module('myApp.portfolio', ['ngRoute'])
     }
     if (searchQuery) {
       _timeout = $timeout(function(){
-        console.log("bark bark");
         $scope.searchStock = StockFactory.getYQL( yqlBuilder('"' + searchQuery + '"'));
         $scope.searchStock.then(function (stock) {
-          console.log(stock)
           $scope.searchStock = stock.query.results.quote;
-          console.log($scope.searchStock);
-          if ($scope.searchStock.Ask === null) {
-            $scope.searchStock = {}
-          } else {
-            $scope.invalidStock = false;
-          }
+          if ($scope.searchStock.Ask === null) { $scope.searchStock = {}
+          } else { $scope.invalidStock = false; }
         })
-
-        //TODO actually search for the stock. Return Price, Change Voluem Mkt Cap last trade & invalidstock = false
         _timeout = null;
       },800);
     }
@@ -72,13 +73,9 @@ angular.module('myApp.portfolio', ['ngRoute'])
   };
   
   $scope.submit = function() {
-      $alert({
-        content: $scope.buy.bDate + $scope.buy.bPrice + $scope.buy.count + $scope.buy.name,
-        animation: 'fadeZoomFadeDown',
-        type: 'material',
-        duration: 3
-      });
-      $scope.portfolio.owned.unshift($scope.buy);
+    console.log("yaya")
+    for (var attrname in $scope.searchStock) {$scope.buy[attrname] = $scope.searchStock[attrname];}
+    $scope.portfolio.owned.unshift($scope.buy);
   };
 
   var stockModal = $modal({scope: $scope, template: 'partials/modal.html', show: false});
@@ -95,11 +92,12 @@ angular.module('myApp.portfolio', ['ngRoute'])
   $scope.portfolio = {"owned":[{"_id":"56296c3410de11cefb90f5d2","qty":39,"name":"Malone","bPrice":3281.94,"bDate":"2014-01-23"},{"_id":"56296c34ea15eaf10f92df3a","qty":26,"name":"Edwards","bPrice":1206.22,"bDate":"2014-12-03"},{"_id":"56296c34026714f38dafa1af","qty":22,"name":"Bowman","bPrice":2540.44,"bDate":"2014-08-30"},{"_id":"56296c348d2ce898d47cfc49","qty":34,"name":"Walls","bPrice":3868.52,"bDate":"2014-06-22"},{"_id":"56296c342b4a8c4e52dff602","qty":26,"name":"Rich","bPrice":3828.78,"bDate":"2014-02-13"},{"_id":"56296c34793e84cae7c1a195","qty":27,"name":"Mcintosh","bPrice":3110.16,"bDate":"2014-03-26"}],"history":[{"_id":"56296c34c28a3abd315aca78","qty":22,"name":"Helga","bPrice":2911.55,"bDate":"2015-10-20","sPrice":1483.17,"sDate":"2015-02-27"},{"_id":"56296c34aaa4faee3df9c2b7","qty":38,"name":"Crane","bPrice":2481.64,"bDate":"2014-01-03","sPrice":2303.64,"sDate":"2014-12-18"},{"_id":"56296c34f139ec319f9a57a8","qty":25,"name":"Beverley","bPrice":1852.6,"bDate":"2015-02-22","sPrice":1250.81,"sDate":"2015-02-05"},{"_id":"56296c344965579628ee1077","qty":27,"name":"Crosby","bPrice":2568.19,"bDate":"2014-03-16","sPrice":2517.54,"sDate":"2015-10-20"},{"_id":"56296c34336f66d11abdb16b","qty":21,"name":"Ratliff","bPrice":3899.37,"bDate":"2014-06-03","sPrice":3328.64,"sDate":"2014-01-20"},{"_id":"56296c340edb4fca4de38166","qty":20,"name":"Angelique","bPrice":1283.67,"bDate":"2014-06-12","sPrice":1376.21,"sDate":"2015-04-18"}]}
 
   //Yahoo query builder
-  var qts = $scope.portfolio.owned.map(function(x) { return '"' + x.name + '"'; }).join(', ');
-  yqlBuilder(qts);
-  //TODO search for the stocks, add the current price to their objects
+  var qts = $scope.portfolio.owned.map(function(x) { return x.name;}).join(', ');
+  $scope.ownedStock = StockFactory.getYQL( yqlBuilder('"' + qts + '"'));
+  $scope.ownedStock.then(function (stock) {
+    arrayObjMerger($scope.portfolio.owned, stock.query.results.quote) 
+  })
 
 }]);
 
 })();
-
